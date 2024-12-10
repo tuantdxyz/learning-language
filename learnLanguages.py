4b8f734863e27cc25e1fc6ed16f14467895f32fe
tuantdxyz <tuantdxyz@gmail.com>
2024-12-10 15:00:48 +0700

    learning en v1

--- learnLanguages.py
+++ learnLanguages.py
@@ -8,6 +8,23 @@ import locale
 locale.setlocale(locale.LC_ALL, 'vi_VN.UTF-8')
 
 
+# Kiểm tra và lấy tham số từ dòng lệnh
+if len(sys.argv) > 1:
+    interval = int(sys.argv[1])  # Tham số đầu tiên: interval
+    interval = interval * 60
+else:
+    interval = 30 * 60  # Giá trị mặc định: 30 phút
+
+show_terminal = False  # Giá trị mặc định
+if len(sys.argv) > 2:
+    show_terminal = sys.argv[2].lower() == 'true'  # Chuyển đổi thành boolean
+
+if show_terminal:
+    print("Chạy log trong terminal..")
+else:
+    print("Không log trong terminal..")
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+    
 # Đọc dữ liệu từ file data.txt
 with open('data.txt', 'r', encoding='utf-8') as file:
     all_records = file.readlines()
@@ -25,9 +42,10 @@ def send_notification(title, message):
 
 def fetch_random_records():
     global all_records
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
 
     if len(all_records) < 2:
-        print(f'[{now}] Đã hiển thị hết tất cả các từ vựng. Reset danh sách từ vựng.')
+        print(f'[{now}] Đã hiển thị hết tất cả các từ. Đang reset danh sách từ..')
         with open('data.txt', 'r', encoding='utf-8') as file:
             all_records = file.readlines()
 
@@ -38,15 +56,15 @@ def fetch_random_records():
         send_notification(title, message)
         time.sleep(5)  # Chờ 5 giây
 
-    now = time.strftime('%Y-%m-%d %H:%M:%S')
-    next_notify_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + 300)) # thêm 5 phút
+    now = time.strftime('%Y-%m-%d %H:%M:%S')  # Cập nhật biến now
+    next_notify_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + interval)) 
     print(f'[{now}] Chờ đến lần hiển thị tiếp theo vào {next_notify_time}')
 
     # Loại bỏ các bản ghi đã được chọn khỏi danh sách all_records
     all_records = [record for record in all_records if record not in random_records]
 
 # Lập lịch để thực hiện hàm fetch_random_records xx minute
-schedule.every(30).minutes.do(fetch_random_records)
+schedule.every(interval).seconds.do(fetch_random_records)
 
 # Chạy lần đầu tiên ngay lập tức
 fetch_random_records()
--- run_app.bat
+++ run_app.bat
@@ -3,16 +3,20 @@
 REM Set encoding cho console
 chcp 65001 > nul
 
-echo Wellcome..
+echo Wellcome to learn..
 
 REM Kích hoạt môi trường ảo
 call .\venv\Scripts\activate
 
 REM Cài đặt các thư viện từ requirements.txt nếu cần
 ::pip install -r requirements.txt
-pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -r requirements.txt
+pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -q -r requirements.txt
+
+REM Cấu hình
+set /p interval="Mặc định 30 phút nếu không nhập. Nhập khoảng thời gian (phút): "
+set /p show_terminal="Mặc định không hiển thị log. Hiện terminal? (true/false): "
 
 REM Chạy script Python
-py .\learnLanguages.py
+py .\learnLanguages.py %interval% %show_terminal%
 
 pause
